69961eafcfcaf06ecc0399f2a626743976300c31
Juansubas <juansubas@gmail.com>
2022-11-16 01:05:51 -0500

    ngModel

---
+++ Angular-U/01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent{
+  heroes: string[] = ['SpiderMan', 'IronMan', 'Hulk', 'Thor', 'Capitán América'];
+  heroeBorrado: string = '';
+
+
+  borrarHeroe(): void {
+    const heroeBorrado: string = this.heroes.shift() || '';
+    this.heroeBorrado = heroeBorrado;
+  }
+}
+
