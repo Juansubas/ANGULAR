0b7b450b741f157e5825cfc0707e7e64a146ba0f
juansubas <juansubas@gmail.com>
2025-09-19 00:25:39 -0500

    feat: add basic types example in TypeScript with variable declarations and type annotations

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,18 @@
+
+
+let name = 'Strider';
+const lastname = 'Strider';
+let hpPoints: number | string = 95;
+let hpPointss: number | 'FULL' = 95;
+const isAlive : boolean = true;
+
+hpPoints = 'Hola Mundo';
+hpPointss = 'FULL';
+
+console.log({
+  name, lastname, hpPoints, hpPointss, isAlive
+})
+
+export {
+
+};
