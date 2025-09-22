2b0e7265cbe24a62af63cb98901451cea32c10bd
juansubas <juansubas@gmail.com>
2025-09-22 16:23:49 -0500

    classes

---
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,17 @@
+
+
+
+export class Person {
+  public name?: string ;
+  private address: string;
+
+  constructor() {
+    this.name = 'Fernando';
+    this.address = 'New York';
+  };
+}
+
+const ironman = new Person();
+
+console.log(ironman);
+
