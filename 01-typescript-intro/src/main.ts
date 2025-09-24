329bb99ab1ec752977a997a4e579c04718207ebb
juansubas <juansubas@gmail.com>
2025-09-23 23:18:14 -0500

    Composicion VS HERENCIA

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -5,8 +5,9 @@ import './style.css';
 //import './topics/03-function';
 //import './topics/04-function-interface';
 //import './topics/05-basic-destructuring';
-import './topics/06-function-destructuring';
-import './topics/07-import-export';
+//import './topics/06-function-destructuring';
+//import './topics/07-import-export';
+import './topics/08-classes';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -5,21 +5,26 @@ export class Person {
   // public name?: string ;
   // private address: string;
 
-  constructor( public name: string, private address: string) {
+  constructor( 
+    public firstName: string, 
+    public lastName: string,
+    private address: string = 'no address') {
   };
 }
 
-export class Hero extends Person {
+export class Hero {
+
   constructor(
     public alterEgo: string,
     public age: number,
     public realName: string,
+    public person : Person,
   ) {
-    super( realName, 'new york');
   }
 }
 
-const ironman = new Hero('Ironman', 45,  'Tony');
+const tony = new Person("Tony", "Stark", "New York");
+const ironman = new Hero('Ironman', 45,  'Tony', tony);
 
 console.log(ironman);
 
